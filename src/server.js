2a0d04b58b8f0623d5633338606684d559e50a5d
abirbenzaamia <61167478+abirbenzaamia@users.noreply.github.com>
2022-10-06 00:55:29 +0100

    fix: pg connection

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-//require ("./config/db.config")
+require ("./config/db.config")
 require('dotenv').config();
 const app = require('express')();
 const cors = require("cors");
@@ -7,7 +7,7 @@ const routes = require("./routes");
 
 app.use(cors());
 app.use(bodyParser.json())
-//app.use(routes);
+app.use(routes);
 app.get('/', (req, res) => {
     res.send(`<h1>API Works !!!</h1>`)
 });
