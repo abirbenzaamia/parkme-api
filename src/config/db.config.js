552c23d1337467de04c6e3e1a4c6abde4e95eab1
abirbenzaamia <61167478+abirbenzaamia@users.noreply.github.com>
2022-10-06 00:59:20 +0100

    fix: pg connection

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,13 +2,13 @@ const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const connect = () => {
 
-    const hostName = process.env.HOST;
+    const hostName = "ec2-52-212-228-71.eu-west-1.compute.amazonaws.com";
     const userName = process.env.USER;
     const password = process.env.PASSWORD;
-    const database = process.env.DB;
+    const database = "de58pvdtukgkt4";
     const dialect = process.env.DIALECT;
 
-    const sequelize = new Sequelize(database, "postgres", password, {
+    const sequelize = new Sequelize(database, "nwmvgkrhxtaucr", "31e544f84ee380bd0731cdd1c9236dd6351c85e344451695285dcc2a7eaf65ff", {
         host: hostName,
         dialect: "postgres",
         //operatorsAliases: false,
