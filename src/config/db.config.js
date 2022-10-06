b15d8cd4c8b568ab9d9fc29a5b00ec008a626b5e
abirbenzaamia <61167478+abirbenzaamia@users.noreply.github.com>
2022-10-06 12:17:04 +0100

    fix : heroku config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,13 +2,13 @@ const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const connect = () => {
 
-    const hostName = "ec2-52-212-228-71.eu-west-1.compute.amazonaws.com";
+    const hostName = process.env.HOST;
     const userName = process.env.USER;
     const password = process.env.PASSWORD;
-    const database = "de58pvdtukgkt4";
+    const database = process.env.DB;
     const dialect = process.env.DIALECT;
 
-    const sequelize = new Sequelize(database, "nwmvgkrhxtaucr", "31e544f84ee380bd0731cdd1c9236dd6351c85e344451695285dcc2a7eaf65ff", {
+    const sequelize = new Sequelize(database,userName, password, {
         host: hostName,
         dialect: "postgres",
         //operatorsAliases: false,
